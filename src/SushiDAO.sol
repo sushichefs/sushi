26f19ebd58d9faa3ecac2f72515cfa5794a2b306
z0r0z <zrzr8963@gmail.com>
2023-03-03 04:26:20 +0800

    👍 Bump, format, clean up

--- src/SushiDAO.sol
+++ src/SushiDAO.sol
@@ -1,27 +1,68 @@
 // SPDX-License-Identifier: WTFPL
 pragma solidity ^0.8.19;
 
-import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {ERC20Votes} from "solbase/tokens/ERC20/extensions/ERC20Votes.sol";
+import {safeTransfer, safeTransferFrom} from "solbase/utils/SafeTransfer.sol";
 
+/// @title Sushi DAO
+/// @notice A DAO for SUSHI token holders.
+/// @author z0r0z.eth
 contract SushiDAO is ERC20Votes("Sushi DAO", unicode"🍣", 18) {
-    using SafeTransferLib for address;
+    /// -----------------------------------------------------------------------
+    /// DAO Storage
+    /// -----------------------------------------------------------------------
 
-    address internal immutable sushi = 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2;
+    address internal constant SUSHI = 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2;
+
+    bool public paused = true;
+
+    /// -----------------------------------------------------------------------
+    /// Custom Errors
+    /// -----------------------------------------------------------------------
+
+    error Paused();
+
+    /// -----------------------------------------------------------------------
+    /// Constructor
+    /// -----------------------------------------------------------------------
 
     constructor() payable {}
 
-    // Enter the DAO. Pay some SUSHIs. Earn some shares.
+    /// -----------------------------------------------------------------------
+    /// DAO Membership Logic
+    /// -----------------------------------------------------------------------
+
+    /// @notice Enter the DAO. Pay some SUSHIs. Earn some shares.
     function enter(uint256 amount) public payable virtual {
-        sushi.safeTransferFrom(msg.sender, address(this), amount);
+        safeTransferFrom(SUSHI, msg.sender, address(this), amount);
 
         _mint(msg.sender, amount);
     }
 
-    // Leave the DAO. Claim back your SUSHIs.
+    /// @notice Leave the DAO. Claim back your SUSHIs.
     function leave(uint256 amount) public payable virtual {
-        sushi.safeTransfer(msg.sender, amount);
+        safeTransfer(SUSHI, msg.sender, amount);
 
         _burn(msg.sender, amount);
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Transferability Logic
+    /// -----------------------------------------------------------------------
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        if (paused) {
+            revert Paused();
+        }
+
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        if (paused) {
+            revert Paused();
+        }
+
+        return super.transferFrom(from, to, amount);
     }
 }
